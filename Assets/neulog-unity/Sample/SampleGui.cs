a1c43e820467601a5448944ecfad1bb921b73e74
Kyle Qian <kylecqian@gmail.com>
2018-06-05 23:18:40 -0700

    NeulogManager

---
+++ Assets/neulog-unity/Sample/SampleGui.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SampleGui : MonoBehaviour
+{
+    string displayText;
+
+    void Update()
+    {
+        displayText = NeulogManager.Instance.BreathReading.ToString();
+    }
+
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+        GUIStyle style = new GUIStyle();
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 6 / 100;
+        style.normal.textColor = new Color(0, 0, 0, 1.0f);
+        GUI.Label(rect, displayText, style);
+    }
+}
---
+++ Assets/neulog-unity/Scripts/NeulogManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[Serializable]
+public class NeulogData
+{
+    public int[] GetSensorValue;
+
+    public static int Parse(string jsonString)
+    {
+        return JsonUtility.FromJson<NeulogData>(jsonString).GetSensorValue[0];
+    }
+}
+
+public class NeulogManager : MonoBehaviour
+{
+    // Singleton reference
+    public static NeulogManager Instance;
+
+    public float BreathReading { get; private set; }
+    [SerializeField] int requestsPerSecond;
+    const string NEULOG_API_URL = "http://localhost:22002/NeuLogAPI?GetSensorValue:[Respiration],[1]";
+
+    void Awake()
+    {
+        Instance = this;
+
+        // Set off method to periodically ping Neulog API
+        InvokeRepeating("QueryNeulog", 0, 1.0f / requestsPerSecond);
+    }
+
+    void QueryNeulog()
+    {
+        UnityWebRequest req = UnityWebRequest.Get(NEULOG_API_URL);
+        StartCoroutine(HandleNeulog(req));
+    }
+
+    IEnumerator HandleNeulog(UnityWebRequest req)
+    {
+        yield return req.SendWebRequest();
+
+        if (req.isNetworkError || req.isHttpError)
+        {
+            Debug.LogWarning(req.error);
+        }
+        else
+        {
+            BreathReading = NeulogData.Parse(req.downloadHandler.text);
+        }
+    }
+}
